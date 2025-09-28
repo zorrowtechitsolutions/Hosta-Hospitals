1a747fd10ad4c708992c9c37a849acd3d8be56b6
Zorrow Tech It Solutions <186459227+zorrowtechitsolutions@users.noreply.github.com>
2025-09-28 14:47:57 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,13 @@
-import { useEffect } from "react";
 import EmailLogin from "./EmailLogin";
-import HospitalOtpLogin from "./OtpLogin";
-
 
 const HospitalLogin: React.FC = () => {
     
-  const userId  = localStorage.getItem("userId")
-
-  useEffect(() => {
-
-  }, [userId])
 
   return (
     <>
-      {
-        userId ?  <EmailLogin /> : <HospitalOtpLogin />
-      }
+
+     <EmailLogin />  
+      
     </>
   );
 };
