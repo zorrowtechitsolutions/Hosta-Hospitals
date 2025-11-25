b7417caa35dcf65f1b95b98d268858bb1acf5e28
FARHAN <farhan@FARHANs-MacBook-Air.local>
2025-11-25 11:19:32 +0530

    feat: ios error

--- src/appRoutes/Routes.jsx
+++ src/appRoutes/Routes.jsx
@@ -1,45 +1,7 @@
-// import { SignIn } from "@/components/auth/Sign-in";
-// import { SignUp } from "@/components/auth/Sign-up";
-// import DashboardPage from "@/dashboard/Dashboard";
-// import DoctorsPage from "@/dashboard/doctors/Doctors";
-// import DashboardLayout from "@/dashboard/layout";
-// import NotificationsPage from "@/dashboard/notification/Notification";
-// import ProfilePage from "@/dashboard/profile/Profile";
-// import SecurityPage from "@/dashboard/security/Security";
-// import SettingsPage from "@/dashboard/settings/Settings";
-// import SpecialtiesPage from "@/dashboard/specialties/Specialties";
-// import ProtectedRoutes from "@/utils/ProtectedRoutes";
-// import React, { useState } from "react";
-// import { Route, Routes } from "react-router-dom";
-
-
-// export default function AppRoutes() {
-
-
-//   return (
-//     <Routes>
-//       <Route path="/sign-in" element={<SignIn  />} />
-//             <Route path="/sign-up" element={<SignUp   />} />
-//       <Route element={<ProtectedRoutes />}>
-//         <Route element={<DashboardLayout />}>
-//           <Route path="/" element={<DashboardPage />} />
-//           <Route path="/dashboard/doctors" element={<DoctorsPage />} />
-//            <Route  path='/dashboard/notifications' element= {<NotificationsPage />} />
-//           <Route path="/dashboard/specialties" element={<SpecialtiesPage />} />
-//           <Route path="/dashboard/profile" element={<ProfilePage />} />
-//           <Route path="/dashboard/settings" element={<SettingsPage />} />
-//           <Route  path='/dashboard/security' element= {<SecurityPage />} />
-//         </Route>
-//       </Route>
-//     </Routes>
-//   );
-// }
-
-
 import { SignIn } from "@/components/auth/Sign-in";
 import { SignInOtp } from "@/components/auth/Sign-in-otp";
 import { SignUp } from "@/components/auth/Sign-up";
-import HospitalHomePage from "@/components/home";
+import HospitalHomePage from "@/components/Home";
 import BookingsPage from "@/dashboard/bookings/Bookings";
 import DoctorBookingPage from "@/dashboard/bookings/doctor-booking/Doctorbooking";
 import DashboardPage from "@/dashboard/Dashboard";
