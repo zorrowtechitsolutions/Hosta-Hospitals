0818e304622e06362ceec5adfbffb73e4223985b
Zorrow Tech It Solutions <zorrowtech@gmail.com>
2026-05-12 13:21:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,11 @@
-// 'use client'
+'use client'
 
 
-// import AppRoutes from './appRoutes/Routes'
-
-// export default function App() {
-//   return (
-//      < AppRoutes />
-
-//   )
-// }
-
-
-import axios from "axios";
-import React, { useState } from "react";
+import AppRoutes from './appRoutes/Routes'
 
 export default function App() {
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = async (e)  => {
-    e.preventDefault();
-
-    console.log("Email:", email);
-    console.log("Password:", password);
-   const response = await axios.post("https://zorrowtek.in/api/hospital/login", {
-    email,
-    password
-   },   {
-    withCredentials: true,
-  });
-
-   console.log(response, "iiiii");
-   
-    // API call here
-  };
-
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <form
-        onSubmit={handleSubmit}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "10px",
-          width: "300px",
-        }}
-      >
-        <h2>Email Login</h2>
+     < AppRoutes />
 
-        <input
-          type="email"
-          placeholder="Enter email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          style={{
-            padding: "10px",
-          }}
-        />
-
-        <input
-          type="password"
-          placeholder="Enter password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          style={{
-            padding: "10px",
-          }}
-        />
-
-        <button
-          type="submit"
-          style={{
-            padding: "10px",
-            cursor: "pointer",
-          }}
-        >
-          Login
-        </button>
-      </form>
-    </div>
-  );
+  )
 }
